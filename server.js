a348370177f51ac26739778223306f6d758d40b7
Quang Minh <minhqnd@gmail.com>
2025-09-05 20:51:51 +0700

    Refactor server.js: Update greeting message and remove unnecessary listen call

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+// app.js
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Xin chào, Node.js app chạy qua Apache!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Passenger không cần listen(3000). 
+// Chỉ cần export app để Passenger hook vào.
+module.exports = app;
