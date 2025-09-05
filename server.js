7d8cbc83b29c4ee1c86afabf1d955ccfd6fd5f67
Quang Minh <minhqnd@gmail.com>
2025-09-05 22:15:33 +0700

    del

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// app.js
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Xin chào, Node.js app chạy qua Apache!");
+  res.send("Hello từ Node.js!");
 });
 
-// Passenger không cần listen(3000). 
-// Chỉ cần export app để Passenger hook vào.
-module.exports = app;
+module.exports = app; // BẮT BUỘC
